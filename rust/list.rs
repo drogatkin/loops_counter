d632bfc5822588689fdad9ef753dcafbc9d3910d
dmitriy <jaddressbook@gmail.com>
2022-10-16 16:36:18 -0700

    added decorations

--- rust/list.rs
+++ rust/list.rs
@@ -1,9 +1,34 @@
 use std::cmp::Ordering;
-use std::io::{self, BufRead};
+//use std::io::{self, BufRead};
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct Node<T> {
   pub val: T,
   pub next: Option<Box<Node<T>>>
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum List<'LI, DATA> {
+    Node {
+        val: DATA,
+        next: &'LI List<'LI, DATA>,
+    },
+    Tail
+}
+
+
+impl<DATA> Default for List<'_, DATA> {
+    fn default() -> Self {
+        List::Tail
+    }
+}
+
+
+
+impl<'LI, DATA> List<'LI, DATA> {
+    pub fn add(&'LI self, val: DATA) -> Self {
+        List::Node { val, next: self }
+    }
 }
 
 impl Node<String> {
@@ -31,6 +56,37 @@ pub fn merge_two_lists(l1: Option<Box<Node<String>>>, l2: Option<Box<Node<String
   }
 }
 
+
+
+pub struct ListIter<'li, DATA>(&'li List<'li, DATA>);
+
+impl<'li, NODE> Iterator for ListIter<'li, NODE> {
+    type Item = &'li NODE;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match &self.0 {
+            List::Node { val, next } => {
+                self.0 = next;
+                Some(val)
+            }
+            List::Tail => None,
+        }
+    }
+}
+
+
+
+impl<'no> Iterator for &'no Node<String> {
+     type Item = &'no Node<String>;
+    fn next(&mut self) -> Option<Self::Item> {
+        match &self.next {
+            None => None,
+            Some(e) => {let &mut res=self;   Some(res)},
+        }
+    }
+}
+
+
 fn print_list(l: Option<Box<Node<String>>>) {
     match l {
         None => println!("."),
@@ -53,7 +109,7 @@ fn main() {
      let mut val : Option<Box<Node<String>>>;
 
     let res = merge_two_lists(val1, val2);
-   // print_list(res);
+    println!("Enter some list values, 'q' for end");
     val = res;
     loop {
         let mut input = String::new();
@@ -69,5 +125,23 @@ fn main() {
     std::io::stdin().read_line(&mut input).expect("Failed to read input");
     find_and_print(input.trim().to_string(), val);
     //print_list(val);
+    
+    let tail = List::Tail;
+    let first = tail.add("apple".to_string());
+    let second = first.add("orange".to_string());
+
+    assert_eq!(tail, List::Tail);
+    assert_eq!(first, List::Node {
+        val: "apple".to_string(),
+        next: &List::Tail,
+    });
+    assert_eq!(second, List::Node {
+        val: "orange".to_string(),
+        next: &List::Node {
+            val: "apple".to_string(),
+            next: &List::Tail,
+        },
+    });
+    
    println!("end")
 }
