7883c56fbb5ce86b621e6241a40a9edc7c515241
dmitriy <jaddressbook@gmail.com>
2022-10-25 18:17:52 -0700

    version with some output

--- rust/hexviw.rs
+++ rust/hexviw.rs
@@ -4,8 +4,9 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::io::{self, Write, prelude::*, SeekFrom};
 use std::fs::File;
+use std::str;
 
-fn main() {
+fn main() -> io::Result<()> {
     println!("HexViw  (c) Copyright {}", 2022);
     let star = String::from("*");
     let args: Vec<String> = env::args().collect();
@@ -41,16 +42,65 @@ fn main() {
            // println!("Selected file : {:?}", pathsVer[num]);
             let file2 = pathsVer[num].as_path().display().to_string(); // into_os_string().into_string().unwrap()
             println!("Selected file : {}", file2);
-            let mut buf = [0_u8;16];
+            let mut buf = [0; 256];
+            let mut strbuf = [0; 16];
            // let mut f = File::open(file2.to_owned().into());
-           let mut f = io::Cursor::<Vec<u8>>::new(file2.to_owned().into());
-            f.read_exact(&mut buf[..16]).expect("read failed");
-           // println!("{}", hex::encode(&buf));
-            for byte in buf {
-              print!("{:02X} ", byte);
+           //let mut f = io::Cursor::<Vec<u8>>::new(file2.to_owned().into());
+           let mut f = File::open(file2)?;
+           let mut remain : i64 = 0;
+           let mut offset = 0;
+           let mut counter : i64 = 0;
+           let mut byteCnt = 0;
+           loop {
+               if remain > 0 {
+                   // print remaining
+                   for byte in buf {  // buf[offset..offset+remain]
+                        if byteCnt == 0 {
+                            print!("\n{:0>8}: ", counter);
+                        }
+                       if offset == 0 {
+                            print!("{:02X} ", byte);
+                            match byte {
+                                0x0a | 0x0d => strbuf[byteCnt] = 0x2e,
+                                _ => strbuf[byteCnt] = byte,
+                            }
+                            //strbuf[byteCnt] = byte;
+                            remain -= 1;
+                            byteCnt += 1;
+                            if byteCnt == 16 {
+                                byteCnt = 0;
+                                let s = str::from_utf8(&strbuf).unwrap().to_string();
+                                //let s = String::from_utf8(strbuf.to_vec()).expect("Found invalid UTF-8");
+                                print!(" {}", s);
+                            }
+                            counter += 1;
+                            if remain == 0 {
+                                break;
+                            }
+                        } else {
+                            offset -= 1;
+                        }
+                    }
+               }
+               let n = f.read(&mut buf[..])?; // .expect("failed to create file").
+              // println!("Read - {}", n);
+               if n == 0 {
+                   //println!("eof");
+                   for _ in 0..16-byteCnt {
+                       print!("   ");
+                   }
+                    let s = str::from_utf8(&strbuf[0..byteCnt]).unwrap().to_string();
+                    println!(" {}", s);
+                   break;
+               }
+    
+               remain = n as i64;
+               offset = 0;
+               
             }
         } else {
             println!("Invalid entry - {}", num);
         }
     }
+    Ok(())
 }
