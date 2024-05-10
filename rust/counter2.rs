bc64ca3c85a16ff3d3383224d4fa903b959b6014
dmitriy <dmitriy@mochamail.com>
2024-05-09 18:54:49 -0700

    more work on performance doc

--- rust/counter2.rs
+++ rust/counter2.rs
@@ -1,22 +1,19 @@
 use std::time::{Duration};
 use std::str;
-use std::sync::{Arc};
 use std::thread;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 fn main() {
     println!("Hello, time!");
-    let spinlock = Arc::new(AtomicUsize::new(1));
-
-    let spinlock_clone = Arc::clone(&spinlock);
+    let atomic = AtomicUsize::new(1);
 
     let _thread = thread::spawn(move|| {
         thread::sleep(Duration::new(1, 0));
-        spinlock_clone.store(0, Ordering::Release);
+        &atomic.store(0, Ordering::Relaxed);
     });
     let mut count = 0i32;
     // Wait for the other thread to release the lock
-    while spinlock.load(Ordering::Acquire) != 0 {
+    while atomic.load(Ordering::Relaxed) == 1 {
         count += 1;
     }
 
