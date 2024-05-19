c40166ce6c27f10a177ff0c4ea0d1c83107ece92
dmitriy <dmitriy@mochamail.com>
2024-05-19 12:51:33 -0700

    compl;ete test

--- rust/str_test.rs
+++ rust/str_test.rs
@@ -2,9 +2,11 @@
 use std::fs::File;
 use std::io::Read;
 use std::convert::TryInto;
+use std::time::{SystemTime};
 
 
 fn main() {
+   let sys_time = SystemTime::now();
    let mut exa = ran_str(25);
    println!{ "str - {exa}" };
    exa = ran_str(25);
@@ -15,6 +17,35 @@ fn main() {
    println!{ "str - {exa}" };
    exa = ran_str(235);
    println!{ "str - {exa}" };
+   let ran_vals = [ran_str(6), ran_str(25), ran_str(14), ran_str(235) , ran_str(155)];
+   let mut res = 0;
+   for i in 1..100_000 {
+       let str = match i {
+       1 => 
+           format!("{}", &ran_vals[0]),
+       2 => format!("{}{}",&ran_vals[0], &ran_vals[1]),
+       3 => format!["{}{}{}",ran_vals[1] , &ran_vals[2] , &ran_vals[0]],
+       4 => format!{"{}{}{}{}",ran_vals[3] , &ran_vals[1] , &ran_vals[2] , &ran_vals[0]},
+       5 => format!("{}{}{}{}{}",ran_vals[3] , &ran_vals[1] , &ran_vals[2] , &ran_vals[0] , &ran_vals[4]),
+       _ => {
+           let mut res_str = ran_vals[4].clone();
+           for j in 1..i {
+               res_str.push_str(&ran_vals[j%ran_vals.len()])
+           }
+           res_str
+       }
+       }; 
+       if let Some(pos) = str.find("A") {
+           res += pos
+       }
+       if let Some(pos) = str.rfind("z") {
+           res += pos
+       }
+   }
+   println!{ "Result: - {res}" };
+   if let Ok(elapsed) = sys_time.elapsed() {
+         println!("Finished in {}.{:<03} sec(s)", elapsed.as_secs(), elapsed.subsec_millis())
+   }
 }
 
 fn ran_str(l:usize) -> String {
@@ -36,5 +67,4 @@ fn gen_rand() -> u32 {
     f.read_exact(&mut buf).unwrap();
     let res = u32::from_be_bytes(buf[0..4].try_into().unwrap());
     res % 64
-    //3
 }
